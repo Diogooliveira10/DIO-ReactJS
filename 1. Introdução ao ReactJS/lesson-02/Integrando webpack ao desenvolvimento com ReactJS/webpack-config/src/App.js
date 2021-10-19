f202740b4346b10d1f57cd5702ee1628e162dfe0
Diogo Oliveira <ddiogooliveira9@gmail.com>
2021-10-18 21:23:52 -0300

    Aprendendo a configurar o webpack

---
+++ 1. Introdução ao ReactJS/lesson-02/Integrando webpack ao desenvolvimento com ReactJS/webpack-config/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Button from './Button'
+
+const App = () => {
+    return (
+        <div>
+            <p>Digital Innovation One</p>
+            <p>Aprendendo a configurar o webpack</p>
+            <div>Webpack 4</div>
+        </div>
+    )
+}
+
+export default App
